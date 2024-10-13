bb04ac2ba3f94778147fb2ba8e4d4945912775c6
nsrddyn@gmail.com <nsrddyn@gmail.com>
2024-10-13 21:39:17 +0200

    klaar met h1

--- module3/h1.mjs
+++ module3/h1.mjs
@@ -2,22 +2,36 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
+let space = " ";
 let ster = "*";
-let rij = 3
-
+let rij = 3;
 
 for (let i = 1; i <= rij; i++) {
-    let output = " ";
+    let output = "";
 
-    for (let j = 1; j <= rij; j++) {
-        output += space;
-
+    for (let j = 1; j <= rij - i; j++) {
+       output += space;
     }
+
     for (let k = 1; k <= (2 * i - 1); k++) {
-        output += ster;
-
+       output += ster;
     }
-    console.log(output)
+
+    console.log(line);
 }
 
-process.exit()
+for (let i = rij - 1; i >= 1; i--) {
+    letoutput = "";
+
+    for (let j = 1; j <= rij - i; j++) {
+       output += space;
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+       output += ster;
+    }
+
+    console.log(line);
+}
+
+userInput.close();
