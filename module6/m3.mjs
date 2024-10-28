272f10a09d2bef926e835296feed5e1bae3fc777
Abdellah El Morabit <nsrddyn@gmail.com>
2024-10-28 13:52:33 +0100

    m3 gedaan

---
+++ module6/m3.mjs
@@ -0,0 +1,18 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
+
+let namen = ["jonas", "thomas", "mehdi", "sanda", "lotte"];
+console.log(namen);
+let naam = await userInput.question("Van welke naam wil je de locatie weten?: ");
+
+function index(namen, naam) {
+	for (let i = 0; i <= namen.length; i++) {
+		if (namen[i] == naam) {
+			return i + 1;
+		}
+	}
+}
+
+console.log(index(namen,naam));
+process.exit()
