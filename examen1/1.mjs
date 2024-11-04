cb1ecdfbe8479f83f8a7d8034439a168b961c11b
--globalnasrlol <nsrddyn@gmail.com>
2024-11-04 18:46:30 +0100

    oefening van het voorbeeld examen verbeterd

--- examen1/1.mjs
+++ examen1/1.mjs
@@ -1,9 +1,10 @@
-for (let i = 0; i < 4; i++){
+// verbetering van een oefening op het deel examen
+// mijn excuus voor het zo slecht maken was dat ik onderbroken werd door vrienden!!!!
 
-for (let j = 0; j <= i; j++){
-
-console.log(j);
-
-}
-
+for (let i = 1; i <= 4; i++) {
+	let output = "";
+	for (let j = 1; j <= i; j++) {
+		output += j;
+	}
+	console.log(output);
 }
